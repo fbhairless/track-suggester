700028df5b07b77df0a4e936d8d13660f956b801
hbf <hbfairless@gmail.com>
2016-11-04 13:01:37 -0700

    add checkbox for toppings to allow for multiple toppings, as individual variables (stage 1)

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 
-function Pizza(qty, size, toppings) {
+function Pizza(qty, size, topping1, topping2) {
   this.quantity = qty;
   this.size = size;
-  this.toppings = toppings;
+  this.topping1 = topping1;
+  this.topping2 = topping2;
+  console.log("topping = " + topping1 + "  " + topping2);
 }
 
-Pizza.prototype.pizzaPrice= function(quantity, size, toppings) {
-  var totalPrice = quantity * (size + toppings);
+Pizza.prototype.pizzaPrice= function(quantity, size, topping1, topping2) {
+  var totalPrice = quantity * (size + topping1 + topping2);
   console.log("quantity = " + quantity);
   console.log("size = " + size);
-  console.log("topping = " + toppings);
+  console.log("topping = " + topping1 + "  " + topping2);
   return totalPrice;
 }
 
@@ -17,11 +19,12 @@ $(document).ready(function(){
   $("form#pizza-info").submit(function(event){
     event.preventDefault();
 
-    var  quantity= parseInt($('input[name="qty-choices"]:checked').val());
+    var quantity= parseInt($('input[name="qty-choices"]:checked').val());
     var size = parseFloat($('input[name="size-choices"]:checked').val());
-    var toppings = parseFloat($('input[name="top-choices"]:checked').val());
-    var newPizza = new Pizza(quantity, size, toppings);
-    var totalPrice = newPizza.pizzaPrice(quantity, size, toppings);
+    var topping1 = parseFloat($('input[name="top-choice1"]:checked').val());
+    var topping2 = parseFloat($('input[name="top-choice2"]:checked').val());
+    var newPizza = new Pizza(quantity, size, topping1, topping2);
+    var totalPrice = newPizza.pizzaPrice(quantity, size, topping1, topping2);
 
     $(".pizza-price").append("<span>" + totalPrice + "</span>");
 
