f774eb55d0bb15bfb45223ff9f3975b1f7e0289d
hbf <hbfairless@gmail.com>
2016-11-04 13:59:44 -0700

    add additional topping

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
 
-function Pizza(qty, size, topping1, topping2) {
+function Pizza(qty, size, topping1, topping2, topping3) {
   this.quantity = qty;
   this.size = size;
   this.topping1 = topping1;
   this.topping2 = topping2;
-  console.log("topping = " + topping1 + "  " + topping2);
+  this.topping3 = topping3;
+
+  console.log("topping = " + topping1 + "  " + topping2 + "  " + topping3);
 }
 
-Pizza.prototype.pizzaPrice= function(quantity, size, topping1, topping2) {
-  var totalPrice = quantity * (size + topping1 + topping2);
+Pizza.prototype.pizzaPrice= function(quantity, size, topping1, topping2, topping3) {
+  var totalPrice = quantity * (size + topping1 + topping2 + topping3);
   console.log("quantity = " + quantity);
   console.log("size = " + size);
-  console.log("topping = " + topping1 + "  " + topping2);
+  console.log("topping = " + topping1 + "  " + topping2 + "  " + topping3);
   return totalPrice;
 }
 
@@ -23,8 +25,9 @@ $(document).ready(function(){
     var size = parseFloat($('input[name="size-choices"]:checked').val());
     var topping1 = parseFloat($('input[name="top-choice1"]:checked').val());
     var topping2 = parseFloat($('input[name="top-choice2"]:checked').val());
-    var newPizza = new Pizza(quantity, size, topping1, topping2);
-    var totalPrice = newPizza.pizzaPrice(quantity, size, topping1, topping2);
+    var topping3 = parseFloat($('input[name="top-choice3"]:checked').val());
+    var newPizza = new Pizza(quantity, size, topping1, topping2, topping3);
+    var totalPrice = newPizza.pizzaPrice(quantity, size, topping1, topping2, topping3);
 
     $(".pizza-price").append("<span>" + totalPrice + "</span>");
 
